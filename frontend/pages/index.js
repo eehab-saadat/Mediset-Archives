2e11e138292b1345b1a16e09d07a7e5d1ad944e7
1Zain <zainiqbal.35201@gmail.com>
2024-05-03 00:17:48 +0500

    card component implemented

---
+++ frontend/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import homeStyles from '../styles/Home.module.css';
+
+import Card from '../components/CardComponent';
+export default function Home() {
+  return (
+    <>
+    <div className={homeStyles.container}>
+      <div className={homeStyles.CardContainer}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+      </div>
+    </div>
+    </>
+
+);
+
+}
---
+++ frontend/pages/posts/first-post.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <>
+    <Layout>
+        <Head>
+          <title>First Post</title>
+          <script src="https://connect.facebook.net/en_US/sdk.js" />
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
+    </>
+  );
+}
