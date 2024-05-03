eac835b7347516a824e81cb91f91c7b1ec0da049
eehab-saadat <eehab.saadat@gmail.com>
2024-05-04 03:16:45 +0500

    updated backend dir structure

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import homeStyles from '../styles/Home.module.css';
-import SignIN from './SignIn'
+import SignIn from './LogIn'
 import Card from '../components/CardComponent';
 export default function Home() {
   return (
     <>
     <div className={homeStyles.container}>
       <div className={homeStyles.CardContainer}>
-          <SignIN/>
+          <SignIn/>
       </div>
     </div>
     </>
