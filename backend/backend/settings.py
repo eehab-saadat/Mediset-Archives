885a01d49b0aaefb44ca5651f3d1f810d477244f
eehab-saadat <eehab.saadat@gmail.com>
2024-05-10 00:00:12 +0500

    added api documentation and sqlite3 config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,6 +96,14 @@ DATABASES = {
     }
 }
 
+# SQLITE3 CONFIG: IF YOU DON'T HAVE POSTGRESQL INSTALLED, USE THIS CONFIGURATION
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
